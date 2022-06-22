5a2d80d85b983d882f850d9a354d5a9b21c78e87
fiatjaf <fiatjaf@gmail.com>
2022-06-21 21:48:44 -0300

    require user-defined mnemonic on startup, do not hardcode.

--- src/main/scala/Config.scala
+++ src/main/scala/Config.scala
@@ -2,6 +2,8 @@ import com.typesafe.config.{ConfigFactory, Config => TypesafeConfig}
 import net.ceedubs.ficus.Ficus._
 import java.io.File
 
+import fr.acinq.bitcoin.{MnemonicCode}
+
 object Config {
   val datadir = System.getProperty(
     "cliche.datadir",
@@ -16,7 +18,35 @@ object Config {
     .withFallback(ConfigFactory.load())
 
   val network = c.as[String]("cliche.network")
-  val seed = c.as[String]("cliche.seed").split(" ").toList
+  val seed =
+    try {
+      c.as[String]("cliche.seed").split(" ").toList
+    } catch {
+      case _: com.typesafe.config.ConfigException$Missing => {
+        println(
+          "# you must specify your mnemonic seed as `cliche.seed = \"...\"` your `cliche.conf` file."
+        )
+
+        val twelveWords = MnemonicCode
+          .toMnemonics(fr.acinq.eclair.randomBytes(16))
+          .mkString(" ")
+        println(
+          s"# if you don't have a mnemonic, here's one your computer has just generated: '${twelveWords}'"
+        )
+        scala.sys.exit(1)
+      }
+    }
+  try {
+    MnemonicCode.validate(seed)
+  } catch {
+    case _: Throwable => {
+      println(
+        s"# mnemonic seed '${seed.take(1).mkString(" ")}...' is not valid."
+      )
+      scala.sys.exit(1)
+    }
+  }
+
   val compactJSON = c.as[Boolean]("cliche.json.compact")
 
   def print(): Unit = {
